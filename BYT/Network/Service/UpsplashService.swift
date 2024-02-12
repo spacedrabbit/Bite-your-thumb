0a3c8c44f588bef4add785a8beb3c25f328d8660
Louis Tur <louis.tur@gmail.com>
2024-02-12 11:59:09 -0600

    refactor image topics

--- BYT/Network/Service/UpsplashService.swift
+++ BYT/Network/Service/UpsplashService.swift
@@ -13,7 +13,7 @@ final class UpsplashService {
 	
 	class func getRandomImage(size: CGSize, scale: CGFloat) async throws -> UpsplashImage {
 		let params: [String : String] = ["orientation" : "portrait",
-										 "topics" : "bo8jQKTaE0Y",
+										 "topics" : UpsplashTopic.wallpapers.id,
 										 "w" : "\(size.width)",
 										 "h" : "\(size.height)",
 										 "dpr" : "\(Int(scale))"]
@@ -24,7 +24,7 @@ final class UpsplashService {
 	
 	class func getRandomImages(size: CGSize, scale: CGFloat, count: Int = 30) async throws -> [UpsplashImage] {
 		let params: [String : String] = ["orientation" : "portrait",
-										 "topics" : "bo8jQKTaE0Y",
+										 "topics" : UpsplashTopic.wallpapers.id,
 										 "w" : "\(size.width)",
 										 "h" : "\(size.height)",
 										 "dpr" : "\(Int(scale))",
@@ -36,3 +36,17 @@ final class UpsplashService {
 	
 }
 
+// It'll make sense to move this into it's own Mappable object type if I want to make new backgrounds available
+fileprivate struct UpsplashTopic {
+	let id: String
+	let name: String
+	
+	// These are just some hardcoded values I got from requesting GET /topics from the upsplash API
+	static let wallpapers = UpsplashTopic(id: "bo8jQKTaE0Y", name: "Wallpapers")
+	static let coolTones = UpsplashTopic(id: "iXRd8cmpUDI", name: "Cool Tones")
+	static let nature = UpsplashTopic(id: "6sMVjTLSkeQ", name: "Nature")
+	static let renders3D = UpsplashTopic(id: "CDwuwXJAbEw", name: "3D Renders")
+	static let textures = UpsplashTopic(id: "iUIsnVtjB0Y", name: "Textures")
+	static let streetPhotography = UpsplashTopic(id: "xHxYTMHLgOc", name: "Street Photography")
+	static let experimental = UpsplashTopic(id: "qPYsDzvJOYc", name: "Experimental")
+}
