ba68eff97a8b4fd6b677fbea8f808362d717e392
Louis Tur <louis.tur@gmail.com>
2024-02-28 12:30:37 -0600

    constraints helpers

---
+++ BYT/Utility/ConstraintsHelper.swift
@@ -0,0 +1,28 @@
+//
+//  ConstraintsHelper.swift
+//  BYT
+//
+//  Created by Louis Tur on 2/28/24.
+//  Copyright © 2024 AccessLite. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+	func constrainBounds(to otherView: UIView) -> [NSLayoutConstraint] {
+		guard let superview,
+				otherView.superview === superview
+				|| superview === otherView else { return [] }
+		stripAutoResizingMasks([self, otherView])
+		
+		return [
+			self.centerYAnchor.constraint(equalTo: otherView.centerYAnchor),
+			self.centerXAnchor.constraint(equalTo: otherView.centerXAnchor),
+			self.heightAnchor.constraint(equalTo: otherView.heightAnchor),
+			self.widthAnchor.constraint(equalTo: otherView.widthAnchor)
+		]
+	}
+	
+}
+
